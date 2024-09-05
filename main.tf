8310b2303df944fba30e95bb3c901096da7011c1
Amine <mohamedamineghanmi01@gmail.com>
2024-09-05 13:14:48 +0100

    updated

--- main.tf
+++ main.tf
@@ -9,4 +9,8 @@ resource "gitlab_user" "first_user" {
   is_external      = true
   reset_password   = false
 }
-
+resource "gitlab_project" "first_project" {
+  name       = "first_project"
+  visibility_level = "public"
+  owner       = gitlab_user.first_user.id
+}
