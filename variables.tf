77c07974a74ea625494cd5a8ec0326f085ba18be
amineadminterraform <mohamedamineghanmi5@gmail.com>
2024-09-06 12:18:57 +0100

    Update variables.tf
    bootstrapping the variables

--- variables.tf
+++ variables.tf
@@ -1,50 +1,49 @@
-variable "gitlab_name" {
-  description = "The name of the GitLab user"
-  type        = string
-}
-
-variable "gitlab_username" {
-  description = "The username for the GitLab user"
-  type        = string
-}
-
-variable "gitlab_password" {
-  description = "The password for the GitLab user"
-  type        = string
-}
-
-variable "gitlab_email" {
-  description = "The email of the GitLab user"
-  type        = string
-}
 variable "s3_bucket" {
-  description = "The S3 bucket for the Terraform state"
-  type        = string
+  type = string
+  default = "state-terraform-s3"
 }
 
 variable "s3_endpoint" {
-  description = "The endpoint for the S3 service"
-  type        = string
+  type = string
+  default = "http://minio.minio:9000"
 }
 
 variable "s3_access_key" {
-  description = "The access key for S3"
-  type        = string
+  type = string
+  default = "console"
 }
 
 variable "s3_secret_key" {
-  description = "The secret key for S3"
-  type        = string
+  type = string
+  default = "console123"
 }
 
 variable "gitlab_base_url" {
-  description = "The base URL for GitLab"
-  type        = string
+  type = string
+  default = "https://gitlab.example.com:32525/"
 }
 
 variable "gitlab_token" {
-  description = "The personal access token for GitLab"
-  type        = string
+  type = string
+  default = "glpat-56UAHhytbakhqvzFor9B"
 }
 
+variable "gitlab_name" {
+  type = string
+  default = "Mohamed amine"
+}
 
+variable "gitlab_username" {
+  type = string
+  default = "amineadmin"
+}
+
+variable "gitlab_password" {
+  type = string
+  default = "amineadmin123"
+}
+
+variable "gitlab_email" {
+  type = string
+  default = "gitlab@aminos.create"
+}
