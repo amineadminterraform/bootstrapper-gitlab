7d8aa275a3d746f15c2bd3f4524fc6ccd15604dd
amineadminterraform <mohamedamineghanmi5@gmail.com>
2024-09-06 12:03:22 +0100

    Update providers.tf
    hardcoded private values for testing purposes

--- providers.tf
+++ providers.tf
@@ -9,19 +9,18 @@ terraform {
   }
 
   backend "s3" {
-    bucket         = var.s3_bucket
-    endpoint       = var.s3_endpoint
-    access_key     = var.s3_access_key
-    secret_key     = var.s3_secret_key
+    bucket         = "state-terraform-s3"
+    endpoint       = "http://minio.minio:9000"
+    access_key     = "console"
+    secret_key     = "console123"
     region         = "us-east-1"
     skip_credentials_validation = true
     skip_metadata_api_check     = true
     force_path_style            = true
   }
 }
-
 provider "gitlab" {
-  base_url = var.gitlab_base_url
-  token    = var.gitlab_token
+  base_url = "https://gitlab.example.com:32525/"
+  token    = "glpat-56UAHhytbakhqvzFor9B"
   insecure = "true"
 }
